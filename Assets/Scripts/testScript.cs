03be8c605045ee068db917ada722256d120ed3d3
pulkit1011 <pulkitname@gmail.com>
2020-06-25 21:03:30 +0530

    working all

--- Assets/Scripts/testScript.cs
+++ Assets/Scripts/testScript.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using SWNetwork;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class testScript : MonoBehaviour
 {
     SyncPropertyAgent syncPropertyAgent;
     NetworkID networkID;
+    private Color[] _colors;
+    private int currentColor = 0;
+    private RemoteEventAgent _remoteEventAgent;
+    
+    private SpriteRenderer image;
     // Start is called before the first frame update
     void Start()
     {
+        image = FindObjectOfType<SpriteRenderer>();
         syncPropertyAgent = GetComponent<SyncPropertyAgent>();
         networkID = GetComponent<NetworkID>();
+        _colors = new[] {Color.red,Color.green, Color.blue, Color.yellow};
+        _remoteEventAgent = GetComponent<RemoteEventAgent>();
+
     }
     
     // Update is called once per frame
@@ -24,5 +34,21 @@ public class testScript : MonoBehaviour
             Debug.Log(modelIndex);
          
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           _remoteEventAgent.Invoke("changecolor");
+           Debug.Log("cg 1");
+        }
+    }
+
+    public void ChangeColor()
+    {
+        if (currentColor == 3)
+        {
+            currentColor = 0;
+        }
+        Debug.Log("cg 2");
+        image.color = _colors[currentColor++];
     }
 }
