81431808f000e4c9b0334f824db3dceacba2c491
pulkit1011 <pulkitname@gmail.com>
2020-06-25 14:53:38 +0530

    init

---
+++ Assets/testScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SWNetwork;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    SyncPropertyAgent syncPropertyAgent;
+    NetworkID networkID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        syncPropertyAgent = GetComponent<SyncPropertyAgent>();
+        networkID = GetComponent<NetworkID>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            int modelIndex = syncPropertyAgent.GetPropertyWithName("test").GetIntValue();
+            syncPropertyAgent.Modify("test",modelIndex+1);
+            Debug.Log(modelIndex);
+         
+        }
+    }
+}
